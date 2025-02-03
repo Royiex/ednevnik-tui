fb932cba29698f19a7190fcee019cbd757588ff2
Royex <royex@royex>
2025-02-03 23:45:01 +0100

    Make grades tab look nicer, keybind "c" and "e" to collapse and expand grades respectivly

--- tui.py
+++ tui.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, TabPane, TabbedContent, Label
+from textual.widgets import Footer, TabPane, TabbedContent, Label, Collapsible
 from data import get_classes, sanitize_name, get_grades
 # import re
 
@@ -10,6 +10,11 @@ grades_data = get_grades()
 
 class ednevnikTui(App):
 
+    BINDINGS = [
+        ("c", "collapse_or_expand(True)", "Collapse All"),
+        ("e", "collapse_or_expand(False)", "Expand All"),
+    ]
+
     def compose(self) -> ComposeResult:
         yield Footer()
         with TabbedContent():
@@ -18,10 +23,14 @@ class ednevnikTui(App):
                     if original in grades_data:
                         for index, date, grade_info in grades_data[original]["grades"]:
                             unique_id = f"{sanitized}_{date.replace('.', '_')}_{index}"
-                            with TabPane(date, id=unique_id):
+                            with Collapsible(collapsed=True,title=grade_info.get('description'), id=unique_id):
                                 yield Label(f"Date: {date}")
                                 yield Label(f"Grade: {grade_info.get('grade', 'N/A')}")
                                 yield Label(f"Description: {grade_info.get('description', 'No description')}\n")
+
+    def action_collapse_or_expand(self, collapse: bool) -> None:
+        for child in self.walk_children(Collapsible):
+            child.collapsed = collapse
 
 
 if __name__ == "__main__":
